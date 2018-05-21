ea18425ba5e7f1dc3e6f8bb79882a5f2ed7165a6
Olesia <olesia.baginski@gmail.com>
2018-05-21 12:13:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Parent from './Parent';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Parent />, document.getElementById('root'));
+registerServiceWorker();
